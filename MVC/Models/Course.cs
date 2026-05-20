a3d1b3fbce20d3ad2232d93ea9b83866ef75227d
SharlottaL <botanichkagigantika@gmail.com>
2026-05-20 15:23:39 +0300

    Add project files.

---
+++ MVC/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+    namespace MVC.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        ////NP
+    }
+}
---
+++ MVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //////Navig p
+        ///
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
